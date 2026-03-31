58a676969df2503a97199024d3078c9ae64b8515
nemo2022 <amal0haj0@gmail.com>
2026-03-31 10:44:28 +0200

    Update database connection: modify engine creation to use psycopg2 for PostgreSQL compatibility

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL and DATABASE_URL.startswith("postgres"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL.replace("postgresql://", "postgresql+psycopg://"))
 
 SessionLocal = sessionmaker(
     autocommit=False,
